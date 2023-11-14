febd7b130a7bed944a2d8e1ad9874087c5fbe74a
Cauan-G <cauanguimaraes503@gmail.com>
2023-11-14 16:21:37 -0300

    correção de requisição

--- public/main.js
+++ public/main.js
@@ -1,28 +1,27 @@
+const gitHubId = document.querySelector('input[type=text]')
+const btnBuscar = document.querySelector('input[type=button]')
 
-const gitHubId = document.querySelector('input[type=text]');
-const button = document.querySelector('#btn-buscar');
 
-button.onclick = function(){
-    // Coneção com a API
+btnBuscar.onclick = function(){
 
-    fetch('https//api.github.com/user/' + gitHubId.value)
+    fetch('https://api.github.com/users/' + gitHubId.value)
     .then(response => {
         if(response.ok){
             return response.json()
         }
-        else{
-            throw new Error('Erro de requisição');
+        else {
+            throw new Error('Erro na requisição')
         }
+    })
+    .then(data => {
+        document.querySelector('#name').textContent = data.name
+        document.querySelector('#bio').textContent = data.bio
+        document.querySelector('#followers').textContent = data.followers
+        document.querySelector('#location').textContent = data.location
+        document.querySelector('#avatar').src = data.avatar_url
 
     })
-    .then(data =>{
-        document.querySelector('#name').textContent = data.name;
-        document.querySelector('#bio').textContent = data.bio;
-        document.querySelector('#followers').textContent = data.followers;
-        document.querySelector('#location').textContent = data.location;
-
-        document.querySelector('#avatar').src = data.avatar_url
-    }).catch(err =>{
-        throw new Error('Erro ao mostrar os dados');
+    .catch(error => {
+        console.log('Erro: '+ error)
     })
 }
